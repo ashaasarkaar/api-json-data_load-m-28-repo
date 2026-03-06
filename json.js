bb8cfea3a958618786c9e9bc1607e708d6ae459f
ashaasarkaar <ashaasarkaar@gmail.com>
2026-03-06 17:18:07 +0600

    Addedd an object Of Js

---
+++ json.js
@@ -0,0 +1,11 @@
+const person = {
+    name: 'Abul',
+    age: '23',
+    fruit: 'Orange',
+    dish: 'halim',
+    isRich: false,
+    money: 20000,
+};
+console.log(person);
+
+// JSON -> JS Object with Notation
