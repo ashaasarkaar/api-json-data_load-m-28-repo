a97acdb537fb2f23af358b5a4022d90fe032b58b
ashaasarkaar <ashaasarkaar@gmail.com>
2026-03-06 19:53:42 +0600

    JSON, JSON.stringify, JSON.paese

--- json.js
+++ json.js
@@ -6,6 +6,19 @@ const person = {
     isRich: false,
     money: 20000,
 };
-console.log(person);
+console.log(person, typeof person);
 
 // JSON -> JS Object with Notation
+// JSON.stringify() method use korbo
+//JSOn.strigify() -> JSON
+//JSON.parse -> object
+
+const personJSON = JSON.stringify(person);  // aikhane object take string a convert koreche JSON
+// JSON sob kisu k notation(double Quation) ar moddhe nei, shudhu boolean value r number k chara. 
+console.log(personJSON, typeof personJSON);
+
+// convert JSON string to Object again
+const parseJSON = JSON.parse(personJSON); 
+console.log(parseJSON, typeof parseJSON); // JSON string k abar object a convert korbe
+
+//JSON holo object ar akta bishesh method
