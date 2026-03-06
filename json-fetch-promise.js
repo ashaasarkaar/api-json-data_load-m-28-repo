6a487e107a1cb38991f570fd274e6c6054dff912
ashaasarkaar <ashaasarkaar@gmail.com>
2026-03-06 21:30:27 +0600

    ftech json chechk

--- json-fetch-promise.js
+++ json-fetch-promise.js
@@ -1,8 +1,22 @@
 //JSON Promise
-const output = fetch('https://jsonplaceholder.typicode.com/todos/1')   // fetch akta function, ai ta kono kisu return kore na, but wait korte bole deyar jonno
-    //   .then(response => response.json())  // fetch() a chaining function kaj kore
+// const output = fetch('https://jsonplaceholder.typicode.com/todos/1')   // fetch akta function, ai ta kono kisu return kore na, but wait korte bole deyar jonno
+    //   .then(response => response.json())  // fetch() a chaining function kaj kore, fetch server theke data anar ba pathanor sohoj upai
     //   .then(json => console.log(json))
 
-    .then(waitPhase => waitPhase.json())
-    .then(data => console.log(data))  // j kono dhoroner JSON data amra aivabe console.log kore readable korte pari
-    // console.log(output);
+    // .then(waitPhase => waitPhase.json())
+//.then(data => console.log(data))  // j kono dhoroner JSON data amra aivabe console.log kore readable korte pari
+// console.log(output);
+
+//Arrow function with fetch
+// const loadData = () => {
+//     fetch('https://jsonplaceholder.typicode.com/todos/1')
+//         .then(waitPhase => waitPhase.json())
+//         .then(data => console.log(data));
+// };
+
+
+const loadData = () =>{
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+};
