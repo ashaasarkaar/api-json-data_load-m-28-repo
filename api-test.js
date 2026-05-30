0994a24061240731a71bc99dde05f222dd2cb827
ashaasarkaar <ashaasarkaar@gmail.com>
2026-05-30 18:50:08 +0600

    test api fetch

--- api-test.js
+++ api-test.js
@@ -3,3 +3,21 @@ const loadData = () => {
         .then((res) => res.json())
         .then((data) => console.log(data))
 }
+
+const loadPosts = () => {
+    const url = "https://jsonplaceholder.typicode.com/posts";
+    
+
+    fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+        console.log(data);
+        displayPosts(data)
+    })
+}
+
+displayPosts = (posts) =>{
+    posts.forEach((post) => {
+        console.log(post)
+    })
+}
